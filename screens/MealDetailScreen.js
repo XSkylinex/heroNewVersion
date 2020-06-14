b9d7b4423645087f4180683934d60e2bf9816461
Alex <alex1mosh@gmail.com>
2020-06-14 12:03:22 +0300

    working

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -6,6 +6,14 @@ import HeaderButton from '../components/HeaderButton';
 import { MEALS } from '../data/dummy-data';
 import DefaultText from '../components/DefaultText';
 
+const ListItem = props => {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  );
+};
+
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
 
@@ -13,16 +21,16 @@ const MealDetailScreen = props => {
 
   return (
     <ScrollView>
-      <Image/>
-      <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
-            <DefaultText>{selectedMeal.duration}m</DefaultText>
-            <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
-            <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
-          </View>
-          <Text style={styles.title}>Ingredients</Text>
-          <Text>List of ingredients...</Text>
-          <Text style={styles.title}>Steps</Text>
-          <Text>List of steps...</Text>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <DefaultText>{selectedMeal.duration}m</DefaultText>
+        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map(ingredient => <ListItem key={ingredient}>{ingredient}</ListItem>)}
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map(step => <ListItem key={step}>{step}</ListItem>)}
     </ScrollView>
   );
 };
@@ -39,10 +47,26 @@ MealDetailScreen.navigationOptions = navigationData => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  details: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around',
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10,
   }
 });
 

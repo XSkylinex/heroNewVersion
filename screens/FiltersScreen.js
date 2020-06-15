f221e8ac1fdae48e22f9f804f38d90ea0d8716f4
Alex <alex1mosh@gmail.com>
2020-06-15 04:50:23 +0300

    done witch screen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -10,43 +10,63 @@ const FilterSwitch = props => {
     <View style={styles.filterContainer}>
       <Text>{props.label}</Text>
       <Switch
+        trackColor={{ true: Colors.primaryColor }}
+        thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
         value={props.state}
         onValueChange={props.onChange}
-        trackColor={{ true: Colors.primaryColor }}
-        thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''} />
+      />
     </View>
-
+    
   );
 };
 
 
 const FiltersScreen = props => {
+  const { navigation } = props;
+
   const [isGlutenFree, setIsGlutenFree] = useState(false);
   const [isLactoseFree, setIsLactoseFree] = useState(false);
   const [isVegan, setIsVegan] = useState(false);
   const [isVegetarian, setIsVegetarian] = useState(false);
 
-  const saveFilters = () => {
+  const saveFilters = useCallback(() => {
     const appliedFilters = {
-      gultenFree: isGlutenFree,
+      glutenFree: isGlutenFree,
       lactoseFree: isLactoseFree,
       vegan: isVegan,
       isVegetarian: isVegetarian
     };
-    console.log(appliedFilters);
-  };
 
-  useEffect(()=> {
-    props.navigation.setParams({save: saveFilters});
-  });
+    console.log(appliedFilters);
+  }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+
+  useEffect(() => {
+    navigation.setParams({ save: saveFilters });
+  }, [saveFilters]);
 
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Available Filters / Restrictions</Text>
-      <FilterSwitch label='Gluten-free' state={isGlutenFree} onChange={newValue => setIsGlutenFree(newValue)} />
-      <FilterSwitch label='Lactose-free' state={isLactoseFree} onChange={newValue => setIsLactoseFree(newValue)} />
-      <FilterSwitch label='Vegan' state={isVegan} onChange={newValue => setIsVegan(newValue)} />
-      <FilterSwitch label='Vegetarian' state={isVegetarian} onChange={newValue => setIsVegetarian(newValue)} />
+      <FilterSwitch
+        label="Gluten-free"
+        state={isGlutenFree}
+        onChange={newValue => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        label="Lactose-free"
+        state={isLactoseFree}
+        onChange={newValue => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label="Vegan"
+        state={isVegan}
+        onChange={newValue => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label="Vegetarian"
+        state={isVegetarian}
+        onChange={newValue => setIsVegetarian(newValue)}
+      />
     </View>
   );
 };
@@ -57,40 +77,46 @@ FiltersScreen.navigationOptions = navData => {
     headerTitle: 'Filter Meals',
     headerLeft: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item title="Menu" iconName="ios-menu" onPress={() => {
-          navData.navigation.toggleDrawer();
-        }} />
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
       </HeaderButtons>
     ),
-    headerRight:  (
+    headerRight: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item title="Save" iconName="ios-save" onPress={() => {
-          console.log('Saving filters!');
-        }} />
+        <Item
+          title="Save"
+          iconName="ios-save"
+          onPress={navData.navigation.getParam('save')}
+        />
       </HeaderButtons>
-    ),
-  }
+    )
+  };
 };
 
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   title: {
     fontFamily: 'open-sans-bold',
     fontSize: 22,
     margin: 20,
-    textAlign: 'center',
+    textAlign: 'center'
   },
   filterContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     width: '80%',
-    marginVertical: 15,
-  },
+    marginVertical: 15
+  }
 });
 
 
